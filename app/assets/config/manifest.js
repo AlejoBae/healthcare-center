1d4f7a4e060a5331d2d27bc7c0a19def6cebb8fa
AlejoBae <balejo188@gmail.com>
2024-08-06 07:25:48 +0300

    Init

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link popper.js
+//= link bootstrap.min.js
+//= link administrate/application.css
+//= link administrate/application.js
+//= link administrate-field-active_storage/application.css
---
+++ db/seeds.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(
+  full_name: 'John Doe',
+  phone: '2222222222',
+  password: 'password2')
+
+User.create!(
+  full_name: 'Jane Doe',
+  phone: '3333333333',
+  password: 'password3')  
+
+p "Patients are successfully created!"
+
+AdminUser.create!(
+  full_name: 'John Boss',
+  phone: '1111111111',
+  password: 'password1')
+
+p "Admin is successfully created!"
+
+Category.create!(
+  name: 'therapist')  
+
+Category.create!(
+  name: 'cardiologist') 
+
+Category.create!(
+  name: 'urologist')  
+  
+Category.create!(
+  name: 'surgeon') 
+
+p "Categories are successfully created!"
+
+Doctor.create!(  
+  full_name: 'Doc One',
+  phone: '4444444444',
+  password: 'oneone',  
+  category_id: 1)  
+
+Doctor.create!(  
+  full_name: 'Doc Two',
+  phone: '5555555555',
+  password: 'twotwo',
+  category_id: 2)  
+
+Doctor.create!(  
+  full_name: 'Doc Three',
+  phone: '6666666666',
+  password: 'threethree',
+  category_id: 3)      
+
+p "Doctors are successfully created!"
